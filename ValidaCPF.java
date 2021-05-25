16bda382c513e6f1c144adda3f3eb1e356f2eea7
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-05-24 21:38:40 -0300

    Update and rename .gitignore to ValidaCPF.java

---
+++ ValidaCPF.java
@@ -0,0 +1,36 @@
+# ValidaCPF
+ValidaCPF
+package ValidaCPF
+ import java.util.Scanner; 
+// importanto a classe "validaCPF" do pacote "MeuPacote" 
+import MeuPacote.validaCPF
+  
+ public static void main(String[] args) { 
+  Scanner ler = new Scanner(System.in);
+ }
+    
+ String CPF; 
+ String isCPF; 
+ String imprimeCPF; 
+ String validaCPF; 
+
+
+}  
+System.out.printf("informe um CPF"); 
+CPF = ler.next(); 
+    
+}
+System.out.printf("\nResultado:"); 
+// usando os metodos isCPF() e imprimeaCPF() de classe "valideCPF" 
+if(validaCPF.isCPF(CPF)==true) 
+System.out.printf("%s/n,"verificador ValidaCPF verifica ValidaCPF.imprimeCPF(CPF)); 
+} 
+} 
+} 
+return verifica ValidaCPF 
+import junit.framework.TestCase;
+interface package ValidaCPF extends Parent {
+    switch 
+    case  
+     ClassValue verifica ValidaCPF
+}
